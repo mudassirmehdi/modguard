f2a93390cb8dc2eca64c0b4f67d3839ca516c4d5
Mudassir Mehdi <mudssair.madhi5@gmail.com>
2023-06-15 16:45:02 +0500

    add old progress bar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-
-
-
-
 /* Preloader */
 $(window).on('load', function() {
 	var preloaderFadeOutTime = 500;
@@ -14,30 +10,4 @@ $(window).on('load', function() {
 	hidePreloader();
 });
 
-var stringifyObject = require('stringify-object')
-var createKeyframe = require('create-keyframe')
-var insertCSS = require('insert-styles')
 
-function generateKeyframe () {
- // var randomColors = createRandomColors()
-
-  var shakeDistance = Number(
-   (Math.random() * 70).toFixed(0)
-  ) + 30
-
-  var cssKeyframe
-  cssKeyframe = {
-    0: {
-      transform: 0+'deg'
-    },
-    100: { transform: 30+'deg' }
-  }
-  var keyframeObj = createKeyframe(cssKeyframe)
-  insertCSS(keyframeObj.css, {id: 'animaton-tutorial-keyframe'})
-
-  jsonDisplay.innerHTML = `@keyframe ${keyframeObj.name} ` +
-   stringifyObject(cssKeyframe, {indent: '  '})
-  userMessage.style.animation = keyframeObj.name + ' ease 3s infinite'
-}
-
-generateKeyframe();
